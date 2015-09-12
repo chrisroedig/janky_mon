db4a66f9aea299c2c9b21bcd034af224119c005e
Nathan Parry <nparry@gmail.com>
2015-09-11 23:44:16 -0400

    Self important script updates
    * Scope directory changes to relevant commands
    * No need to pull (and hit network again) - just reset to the given SHA

--- script/update.sh
+++ script/update.sh
@@ -1,20 +1,13 @@
-#!/bin/sh
+#!/bin/sh -e
 
 git fetch
 
 LOCAL=$(git rev-parse HEAD )
 REMOTE=$(git rev-parse @{u})
 
-echo $LOCAL
-echo $REMOTE
+[ $LOCAL = $REMOTE ] && echo "up to date ($REMOTE)" && exit 0
 
-if [ $LOCAL = $REMOTE ]; then
-    echo 'up to date....'
-    exit 0;
-fi
-
-echo 'new master detected, pulling now!'
-git pull
+echo "new master detected ($LOCAL -> $REMOTE)!"
+git reset --hard $REMOTE
 ./script/install.sh
 
-exit 1;
