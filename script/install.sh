d00ca641971066dbea42a09d996f245b11f1b2fb
Chris Roedig <CRoedig@covermymeds.com>
2015-12-09 14:44:34 -0500

    install deps on deploy

--- script/install.sh
+++ script/install.sh
@@ -10,5 +10,8 @@ echo 'building rpi_ws281x...'
 echo 'setting up python bindings for rpi_ws281x...'
 (cd libs/rpi_ws281x/python && sudo python setup.py install)
 
+echo 'installing python dependencies'
+pip install requests
+
 echo 'done...'
 
